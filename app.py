25c39f5b9f44fc747d8932a593fc06cd212832c0
Tech EFX Tv <59799893+efxtv@users.noreply.github.com>
2020-11-26 18:17:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,55 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, You Could you please subscribe!</p> <br /> <h2>EEX Tv </h2>"
+    # return "<p>Hello, You Could you please subscribe!</p> <br /> <h2>EEX Tv </h2>"
+
+
+# new file
+eturn "<p>Hello, You Could you please subscribe!</p> <br /> <h2>EEX Tv </h2>
+<!DOCTYPE html>
+<html>
+<head>
+<style> 
+div {
+  width: 100%;
+  height: 10%px;
+ 
+  font-weight: bold;
+  position: relative;
+  animation: mymove 5s infinite;
+}
+
+
+#div1 {animation-timing-function: linear;background-color:green;}
+#div2 {animation-timing-function: ease;}
+#div3 {animation-timing-function: ease-in;}
+#div4 {animation-timing-function: ease-out;}
+#div5 {animation-timing-function: ease-in-out;}
+
+@keyframes mymove {
+  from {left: 0px;}
+  to {left: 100px;}
+  from {right: 100px;}
+  to {right: 0px;}
+}
+.responsive {
+  width: 100%;
+  height: auto;
+}
+</style>
+</head>
+<body>
+
+<p>Hello, You Could you please subscribe!</p> <a href="https://www.youtube.com/channel/UCIWHXkNEHsedIz62jUJwAew" target="_blank"><h3><center>EFX TV</center></h3></a>
+ <a href="https://www.youtube.com/channel/UCIWHXkNEHsedIz62jUJwAew"><img src="https://instagram.fdel10-1.fna.fbcdn.net/v/t51.2885-15/e35/117225185_184373223072603_1003988386057333093_n.jpg?_nc_ht=instagram.fdel10-1.fna.fbcdn.net&_nc_cat=103&_nc_ohc=coiR_mFQ2s0AX-4GlYh&tp=1&oh=76f6860ccce0288501d3f8fc273072fb&oe=5FEA8275" alt="EFX Tv" class="responsive"> 
+
+
+
+<div id="div5"><center>PLEASE</center></div><br>
+<div id="div1"><center>SUBSCRIBE</center></div><br>
+<div id="div5"><center>EFX TV</center></div><br>
+<div id="div5"><center>1 LIKE </center></div><br>
+<div id="div2"><center>FOR THIS TUTORIAL</center></div><br>
+
+</body>
+</html>"
